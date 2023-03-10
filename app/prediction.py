10e0a7799d4e86556e77a933adbb6b5ea4b70720
Rosana <rosanacibely@gmail.com>
2023-03-10 13:45:51 +0000

    feat: added prediction class

---
+++ app/prediction.py
@@ -0,0 +1,24 @@
+import joblib
+import pandas as pd
+import streamlit as st
+from utils import *
+
+class Prediction:
+    def __init__(self, path, user_input_variables):
+        self.path = path
+        
+        self.predict_result(user_input_variables)
+
+    def predict_result(self, user_input_variables):
+        
+        model = joblib.load("./models/RandomForestClassifier.sav")
+        response = model.predict(user_input_variables)
+
+
+        if response == 0:
+            st.info(f"Chances de ter euthyroid: BAIXA")
+        if response == 1:
+            st.warning(f"Chances de ter euthyroid: MÉDIA")
+
+            
+        st.markdown('---')
