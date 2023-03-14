33450fc82111e202e426bc595a51903b5b02cb86
Rosana <rosanacibely@gmail.com>
2023-03-14 19:24:30 +0000

    refactor: refactored the system structre

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ class Main():
         image = Image.open('app/icon/cilab.png')
         st.set_page_config(
             page_title="ESS - Sistema de apoio ao diagnóstico",
-            page_icon=image
-        )
+            page_icon=image,
+            layout="wide",
+            initial_sidebar_state="expanded"
+            )
         
         self.placeholder = st.empty()
 
@@ -22,67 +24,36 @@ class Main():
             self.home()
             
         if st.session_state.page == 1:
-            self.home()
+            Prediction(path="models/RandomForestClassifier.sav")
             st.button("Voltar",on_click=lambda:self.set_page(0))
             
-       
+ 
     def set_page(self,num):
         st.session_state.page=num
         
     def home(self):
         """ Home page """
-   
-        st.title("Sistema de apoio ao diagnóstico")
-        st.header("Síndrome do Eutireoideo doente")
-        col1, col2 = st.columns([1,3]) # duas colunas com tamanhos diferentes 1 e 3
 
+        # set the title 
+        st.markdown('<style>h1{font-size: 30px;}</style>', unsafe_allow_html=True)
+        # set title color 
+        st.markdown('<style>h1{color: #1E90FF;}</style>', unsafe_allow_html=True)
+        st.title("Sistema de apoio ao diagnóstico: Síndrome do Eutireoideo doente")
+       
 
-        with col1:
-            st.text("Insira os dados:")
-            age = st.number_input("Idade",min_value=1, max_value=100, value=1)
-            sex = st.selectbox("Sexo",("F","M"))
-            sick = st.selectbox("Possui algum distúrbio da tireoide?", ("Não", "Sim"))
-            tsh = st.number_input("TSH",min_value=0.0, max_value=100.0, value=0.0)
-            t3 = st.number_input("T3",min_value=0.0, max_value=100.0, value=0.0)
-            tt4 = st.number_input("TT4",min_value=0.0, max_value=100.0, value=0.0)
-            t4u = st.number_input("T4U",min_value=0.0, max_value=100.0, value=0.0)
-            fti = st.number_input("FTI",min_value=0.0, max_value=100.0, value=0.0)
-            sex = sex_string2int(sex)
-            sick = sick_string2int(sick)
+        # set the subtitle 
+        st.markdown('<style>h2{font-size: 20px;}</style>', unsafe_allow_html=True)
+        st.header("Você sabe o que é a sindrome do Eutireoideo doente?")
 
-            user_input_variables = get_user_data(age, sex, sick, tsh, t3, tt4, t4u, fti)
-            st.button("Realizar predição",on_click=lambda:Prediction(path="models/RandomForestClassifier.sav", user_input_variables = user_input_variables))
-            st.button("Limpar", on_click=lambda: clear())
+        # add a image
+        image = Image.open('icon/icon.png')
+        st.image(image, caption='Síndrome do Eutireoideo doente', use_column_width=True)
 
-
-        with col2:
-            st.header("Você sabe o que é a sindrome do Eutireoideo doente?")
-
-            # escrever sobre a sindrome 
-            st.markdown("""<div><p align="justify">A síndrome do doente Eutireoideo é uma condição em que ocorre um distúrbio na regulação hormonal das glândulas tireóideas em pacientes que apresentam alguma outra doença ou infecção (Geomann e Wajner, 2009). Embora a tireoide esteja funcionando normalmente, a sua produção de hormônios tireoidianos é afetada pela inflamação, infecção ou outra condição.</p></div>""", unsafe_allow_html=True) 
-
-        # set a icon image in footer in the center
-        st.text("Realização")
-        st.markdown("<center><img src='https://raw.githubusercontent.com/cilab-ufersa/euthyroid_diagnostic_support_app/main/app/icon/cilab.png' width='200'></center>", unsafe_allow_html=True) 
-        st.markdown("<center><img src='https://raw.githubusercontent.com/cilab-ufersa/euthyroid_diagnostic_support_app/main/app/icon/Ufersa.png' width='70'></center>", unsafe_allow_html=True)
-
-    
-        st.markdown("""
-                        <br/>
-                        <br/>
-                        <center>
-                                <a href="https://github.com/cilab-ufersa/euthyroid_diagnostic_support_app">
-                                    <div class="row-widget stButton" style="width: 600px;">
-                                        <button kind="secondary" class="css-5uatcg edgvbvh10">
-                                            <div data-testid="stMarkdownContainer" class="css-1fv8s86 e16nr0p34">
-                                                <p>Source</p>
-                                            </div>
-                                        </button>
-                                    </div>
-                                </a>
-                        </center>
-                        """,unsafe_allow_html=True)
-     
+        # TODO(Vinicius) escrever sobre a sindrome 
+        st.markdown("""<div><p align="justify">A síndrome do doente Eutireoideo é uma condição em que ocorre um distúrbio na regulação hormonal das glândulas tireóideas em pacientes que apresentam alguma outra doença ou infecção (Geomann e Wajner, 2009). Embora a tireoide esteja funcionando normalmente, a sua produção de hormônios tireoidianos é afetada pela inflamação, infecção ou outra condição.</p></div>""", unsafe_allow_html=True) 
+        # change button color 
+        st.markdown('<style>div.row-widget.stButton > button {color: white; background-color: #1E90FF;}</style>', unsafe_allow_html=True)
+        st.button("Realizar Diagnóstico",on_click=lambda:self.set_page(1))  
         
 
             
