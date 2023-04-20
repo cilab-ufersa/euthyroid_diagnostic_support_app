a541d566af039ae19f67beb3f99bf5f55f358aa7
Rosana <rosanacibely@gmail.com>
2023-04-20 19:39:56 +0000

    fix: fixed path

--- tests/model_test.py
+++ tests/model_test.py
@@ -4,7 +4,7 @@ def test_model():
     """
     Test the model
     """
-    model = joblib.load("/workspaces/euthyroid_diagnostic_support_app/models/RandomForestClassifier.sav")
+    model = joblib.load("../models/StackingClassifier.sav")
     sick= [[45, 0, 0, 1.9, 1.0,	82.0,	0.73,	112.0]]
     not_sick = [[49, 0, 0, 0.8,	2.4,123.0,	0.99,	124.0]]
     assert model.predict(sick)[0] == 1
